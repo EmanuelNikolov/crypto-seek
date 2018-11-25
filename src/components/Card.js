5bd2ec6b86fb623f692e1f62ef65917c4d5d0332
emanuilnikolov <enikolov.intl@gmail.com>
2018-11-25 15:39:47 +0200

    Fix coin name overflow for longer string.

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({id, name, img, price}) => {
     return (
         <div className="card-item header br4 dib p-2 ma2 grow bw2 shadow-5">
             <img className={"pt2"} alt={name} src={fullImageUrl} height='100' width='100'/>
-            <p className={"fw7 pt2"}>{name}</p>
+            <p className={"fw7 pt2 text-truncate"}>{name}</p>
             <p>{price}</p>
             <p>USD</p>
         </div>
